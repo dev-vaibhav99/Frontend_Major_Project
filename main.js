ad031366bb59bb46c99c4fcdff07cc7badeee2e5
Vaibhav Chandrashekhar <dev.vaibhav99@gmail.com>
2022-04-15 14:34:58 +0530

    js for hiding navbar after clicking on a perticular link has addded

--- main.js
+++ main.js
@@ -7,3 +7,13 @@ window.onscroll = function(){
         nav.classList.remove("scroll-on");
     }
 }
+
+
+// navbar hide
+let navbar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navbar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
